bbcd52d92d2f0188e5bac0931201b38fd107c826
Ioannis Giannakaras <71703504+igianGH@users.noreply.github.com>
2025-04-09 02:28:39 +0300

    Update __.py

--- __.py
+++ __.py
@@ -100,7 +100,7 @@ def interpret(file="source",ftrb=False,dline=True,segment=False,report=False,ran
           if(snl==msnl):
             while(line[0]==' '):
               line=line[1:]
-            print(str(snl+1-linecorr)+". ",line,"\n")
+            print(str(snl+1-linecorr)+". ",line,"")
             if(dline):
               line=next(lines)
               snl+=1
